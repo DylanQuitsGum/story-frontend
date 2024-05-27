6cf3a355c2ebd5eb2f6c5c13e2cd2a733a9431f8
Dat Tran <dattran10@gmail.com>
2024-05-27 16:36:05 -0500

    Settings

--- src/router.js
+++ src/router.js
@@ -23,6 +23,11 @@ const router = createRouter({
       component: () => import("./views/Register.vue"),
     },
     {
+      path: "/settings",
+      name: "settings",
+      component: () => import("./views/dashboard/Settings.vue"),
+    },
+    {
       path: "/dashboard",
       name: "dashboard",
       component: Dashboard,
--- src/views/dashboard/Settings.vue
+++ src/views/dashboard/Settings.vue
@@ -1,3 +1,3 @@
 <template>
-  <div>Stories</div>
+  <div>Settings</div>
 </template>
