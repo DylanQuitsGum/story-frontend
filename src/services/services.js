b8e96dfdb4562e9d8b0d6e388c53ae5a3e4d5a3b
Dat Tran <dattran10@gmail.com>
2024-05-27 20:38:55 -0500

    Update baseurl to access backend

--- src/services/services.js
+++ src/services/services.js
@@ -4,7 +4,7 @@ var baseurl = "";
 if (process.env.NODE_ENV === "development") {
   baseurl = "http://localhost:3201/storyapi/";
 } else {
-  baseurl = "http://107.23.63.118:3201/storyapi/";
+  baseurl = "http://localhost:3201/storyapi/";
 }
 
 const apiClient = axios.create({
