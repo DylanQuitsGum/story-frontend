fae29649cc11226e6ad39cef7bba3636ab496ca9
Dat Tran <dattran10@gmail.com>
2024-05-29 20:15:29 -0500

    Submit registration

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -58,7 +58,7 @@ async function createAccount() {
       snackbar.value.value = true;
       snackbar.value.color = "green";
       snackbar.value.text = "Account created successfully!";
-      router.push({ name: "login" });
+      // router.push({ name: "login" });
     })
     .catch((error) => {
       console.log(error);
@@ -76,49 +76,50 @@ function closeSnackBar() {
 
 <template>
   <v-container class="container d-flex align-center justify-center" fluid>
-    <v-card class="card rounded-md elevation-1">
-      <v-card-title class="headline mb-2">Register </v-card-title>
+    <v-form @submit.prevent="createAccount">
+      <v-card class="card rounded-md elevation-1">
+        <v-card-title class="headline mb-2">Register </v-card-title>
 
-      <v-card-text>
-        <v-text-field
-          v-model="user.firstName"
-          label="First Name"
-          variant="outlined"
-          required
-        ></v-text-field>
+        <v-card-text>
+          <v-text-field
+            v-model="user.firstName"
+            label="First Name"
+            variant="outlined"
+            required
+          ></v-text-field>
 
-        <v-text-field
-          v-model="user.lastName"
-          label="Last Name"
-          variant="outlined"
-          required
-        ></v-text-field>
+          <v-text-field
+            v-model="user.lastName"
+            label="Last Name"
+            variant="outlined"
+            required
+          ></v-text-field>
 
-        <v-text-field
-          v-model="user.email"
-          label="Email"
-          variant="outlined"
-          required
-        ></v-text-field>
+          <v-text-field
+            v-model="user.email"
+            label="Email"
+            variant="outlined"
+            required
+          ></v-text-field>
 
-        <v-text-field
-          v-model="user.password"
-          label="Password"
-          variant="outlined"
-          required
-        ></v-text-field>
-      </v-card-text>
-      <v-card-actions>
-        <v-btn
-          class="login-btn rounded-xl"
-          variant="flat"
-          color="primary"
-          @click="createAccount()"
-          >Register</v-btn
-        >
-      </v-card-actions>
-    </v-card>
-
+          <v-text-field
+            v-model="user.password"
+            label="Password"
+            variant="outlined"
+            required
+          ></v-text-field>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn
+            class="login-btn rounded-xl"
+            variant="flat"
+            color="primary"
+            type="submit"
+            >Register</v-btn
+          >
+        </v-card-actions>
+      </v-card>
+    </v-form>
     <v-snackbar v-model="snackbar.value" rounded="pill">
       {{ snackbar.text }}
 
