c9ed305214e5c016efa8fd501ec190a9f55fee94
bluegene92 <dattran10@gmail.com>
2024-06-08 12:52:57 -0500

    Export to pdf

--- src/views/Story.vue
+++ src/views/Story.vue
@@ -6,11 +6,19 @@
 .text {
   white-space: pre-line;
 }
+
+.export-btn {
+  background-color: #41f061;
+  background-image: linear-gradient(160deg, #30ee7f 0%, #2dcc94 100%);
+}
 </style>
 
 <script>
 import { onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
+import jsPDF from "jspdf";
+import "jspdf-autotable";
+
 import StoryServices from "../services/StoryServices";
 
 export default {
@@ -18,7 +26,6 @@ export default {
     const story = ref({});
     const route = useRoute();
     const storyId = ref(route.params.id);
-    console.log(storyId.value);
 
     const fetchStory = async () => {
       const user = JSON.parse(localStorage.getItem("user"));
@@ -35,8 +42,41 @@ export default {
       fetchStory();
     });
 
+    const exportToPDF = () => {
+      const title = document.querySelector("#title");
+      const text = document.querySelector("#text");
+
+      const pdf = new jsPDF();
+      const margin = 20;
+      const pageWidth = pdf.internal.pageSize.getWidth();
+      const pageHeight = pdf.internal.pageSize.getHeight();
+      const maxLineWidth = pageWidth - margin * 2;
+      const lineHeight = 5;
+      let cursorY = margin;
+
+      pdf.setFontSize(20);
+      pdf.text(title.textContent, 10, 10);
+
+      let content = text ? text.textContent : "";
+
+      const lines = pdf.splitTextToSize(content, maxLineWidth);
+      pdf.setFontSize(12);
+
+      lines.forEach((line) => {
+        if (cursorY + lineHeight > pageHeight - margin) {
+          pdf.addPage();
+          cursorY = margin;
+        }
+        pdf.text(line, margin, cursorY);
+        cursorY += lineHeight;
+      });
+
+      pdf.save(`${title.textContent.replace(/ /g, "_")}`);
+    };
+
     return {
       story,
+      exportToPDF,
     };
   },
 };
@@ -47,9 +87,17 @@ export default {
   <div class="page">
     <v-container>
       <div>
-        <h1>{{ story.title }}</h1>
-        <div class="text">
-          {{ story.text }}
+        <div class="d-flex justify-end">
+          <v-btn class="export-btn rounded-xl" @click="exportToPDF"
+            >Export to PDF</v-btn
+          >
+        </div>
+
+        <div id="content">
+          <h1 id="title">{{ story.title }}</h1>
+          <div id="text" class="text">
+            {{ story.text }}
+          </div>
         </div>
       </div>
     </v-container>
